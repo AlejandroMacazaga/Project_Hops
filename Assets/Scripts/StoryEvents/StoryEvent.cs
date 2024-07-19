a5ffcdc864f7e0f77f09eef2b2ab140b99afc7a7
AlejandroMacazaga <50550888+AlejandroMacazaga@users.noreply.github.com>
2024-07-19 12:52:26 +0200

    Added story event system

---
+++ Assets/Scripts/StoryEvents/StoryEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets.Scripts.StoryEvents
+{
+    [System.Serializable]
+    public struct StoryEvent
+    {
+        public string eventName;
+        public bool isUnlocked;
+        public string unlockConditionDescription; // For debugging or design purposes
+    }
+}
--- Assets/Scripts/StoryEvents/StoryManager.cs
+++ Assets/Scripts/StoryEvents/StoryManager.cs
@@ -1,3 +1,5 @@
+using Assets.Scripts.StoryEvents;
+using AYellowpaper.SerializedCollections;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +7,50 @@ using Utils.Singletons;
 
 public class StoryManager : PersistentSingleton<StoryManager>
 {
-    private readonly int[] storyEvents = new int[999];
-    
+    [SerializeField]
+    private List<StoryEvent> storyEvents;
+
+    private void Start() 
+    {
+        LoadProgress();
+    }
+
+    public void UnlockEvent(string eventName)
+    {
+        var storyIndex = storyEvents.FindIndex(e => e.eventName == eventName);
+        if (storyIndex >= 0)
+        {
+            var storyEvent = storyEvents[storyIndex];
+            storyEvent.isUnlocked = true;
+            Debug.Log($"Story Event {eventName} unlocked!");
+            PlayerPrefs.SetInt(eventName, 1);
+            // Trigger the event or handle any additional logic here
+        }
+        else
+        {
+            Debug.LogWarning($"Story Event {eventName} not found.");
+        }
+    }
+
+    public bool IsEventUnlocked(string eventName)
+    {
+        var storyIndex = storyEvents.FindIndex(e => e.eventName == eventName);
+        return storyIndex >= 0 && storyEvents[storyIndex].isUnlocked;
+    }
+
+    private void LoadProgress()
+    {
+        // todo: Load from a save file or database
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveProgress();
+    }
+
+    private void SaveProgress()
+    {
+        // todo: Save to a save file or database
+    }
 
 }
